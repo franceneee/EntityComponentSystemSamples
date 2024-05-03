278e025c8e3be45f54439af1504fef3a5b5539f6
Francene <57038559+franceneee@users.noreply.github.com>
2024-05-03 17:31:54 +0800

    Update CameraController.cs
    allow rotation of camera

--- EntitiesSamples/Assets/CameraController.cs
+++ EntitiesSamples/Assets/CameraController.cs
@@ -15,11 +15,11 @@ public class CameraController : MonoBehaviour
         // Move the camera forward, backward, left, and right
         transform.position += transform.forward * Input.GetAxis("Vertical") * speed * Time.deltaTime;
         transform.position += transform.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-
+        */
         // Rotate the camera based on the mouse movement
         float mouseX = Input.GetAxis("Mouse X");
         float mouseY = Input.GetAxis("Mouse Y");
-        transform.eulerAngles += new Vector3(-mouseY * sensitivity, mouseX * sensitivity, 0); */
+        transform.eulerAngles += new Vector3(-mouseY * sensitivity, mouseX * sensitivity, 0);
 
 
         if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
